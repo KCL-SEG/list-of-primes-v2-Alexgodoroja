b532f909fcc47301d7bab933a14edd781091db2f
Alexandru Godoroja <alexandru.godoroja@kcl.ac.uk>
2022-10-22 20:10:57 +0100

    raise ValueError on improper arguments

--- primes.py
+++ primes.py
@@ -1,6 +1,23 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
-
+from math import sqrt
 def primes(number_of_primes):
     list = []
+    first_prime = 2
+    if number_of_primes < 1:
+        raise ValueError
+    while len(list)<number_of_primes:
+        isprime = True
+        
+        for x in range(2, int(sqrt(first_prime) + 1)):
+            if first_prime % x == 0: 
+                isprime = False
+                break
+        
+        if isprime:
+            list.append(first_prime)
+        
+        first_prime += 1
+    
     return list
+
